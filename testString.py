528cae6ae6b9d029892d4321d3454dea39e9e6a9
Noiccy <parsleyzhou@163.com>
2021-09-23 09:19:16 +0800

    correct testString.py

--- testString.py
+++ testString.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('asd'.upper(),'AS')
+        self.assertEqual('asd'.upper(),'ASD')
 
 if __name__ == '__main__':
     unittest.main()
